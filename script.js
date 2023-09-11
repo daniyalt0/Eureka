879a1f2e5c471be3ecc4ee398ed4965b0f7fc23f
Muhammad Daniyal tariq <97436711+daniyalt0@users.noreply.github.com>
2023-09-12 03:22:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+onst prompts = [
+    "Write a short story about a time traveler.",
+    "Describe your dream vacation destination in detail.",
+    "Create a poem about the changing seasons.",
+    "Imagine a world without technology. How would people live?",
+    "Write a dialogue between two characters who have just met."
+];
 
+const generateButton = document.getElementById("generateButton");
+const promptDisplay = document.getElementById("promptDisplay");
+
+generateButton.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * prompts.length);
+    const randomPrompt = prompts[randomIndex];
+    promptDisplay.textContent = randomPrompt;
+});
