fea6875194063971fc126caaed8f8653509ba3c8
Muhammad Daniyal tariq <97436711+daniyalt0@users.noreply.github.com>
2023-09-13 01:25:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-onst prompts = [
-    "Write a short story about a time traveler.",
-    "Describe your dream vacation destination in detail.",
-    "Create a poem about the changing seasons.",
-    "Imagine a world without technology. How would people live?",
-    "Write a dialogue between two characters who have just met."
+const prompts = [
+    "Prompt 1",
+    "Prompt 2",
+    "Prompt 3",
+    // Add your 30 prompts here
 ];
 
 const generateButton = document.getElementById("generateButton");
 const promptDisplay = document.getElementById("promptDisplay");
 
 generateButton.addEventListener("click", () => {
-    const randomIndex = Math.floor(Math.random() * prompts.length);
-    const randomPrompt = prompts[randomIndex];
-    promptDisplay.textContent = randomPrompt;
+    if (prompts.length === 0) {
+        promptDisplay.textContent = "No prompts remaining.";
+    } else {
+        const randomIndex = Math.floor(Math.random() * prompts.length);
+        const randomPrompt = prompts[randomIndex];
+        promptDisplay.textContent = randomPrompt;
+        prompts.splice(randomIndex, 1); // Remove the used prompt
+    }
 });
