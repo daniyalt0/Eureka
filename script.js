882ea8951f3637c1f0f74f784ed524080a9a2b9e
Muhammad Daniyal tariq <97436711+daniyalt0@users.noreply.github.com>
2023-09-14 05:36:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,166 +1,92 @@
 // Define prompts for each group and prompt list
-
-//g1
-let g1Prompts1 = [
-    "Prompt 1 for Group 1 - List 1",
-    "Prompt 2 for Group 1 - List 1",
-    // Add more prompts for List 1
-];
-
-let g1Prompts2 = [
-    "Prompt 1 for Group 1 - List 2",
-    "Prompt 2 for Group 1 - List 2",
-    // Add more prompts for List 2
-];
-
-let g1Prompts3 = [
-    "Prompt 1 for Group 1 - List 3",
-    "Prompt 2 for Group 1 - List 3",
-    // Add more prompts for List 3
-];
-
-//g2
-let g2Prompts1 = [
-    "Prompt 1 for Group 2 - List 1",
-    "Prompt 2 for Group 2 - List 1",
-    // Add more prompts for List 1
-];
-
-let g2Prompts2 = [
-    "Prompt 1 for Group 2 - List 2",
-    "Prompt 2 for Group 2 - List 2",
-    // Add more prompts for List 2
-];
-
-let g2Prompts3 = [
-    "Prompt 1 for Group 2 - List 3",
-    "Prompt 2 for Group 2 - List 3",
-    // Add more prompts for List 3
-];
-
-//g3
-let g3Prompts1 = [
-    "Prompt 1 for Group 3 - List 1",
-    "Prompt 2 for Group 3 - List 1",
-    // Add more prompts for List 1
-];
-
-let g3Prompts2 = [
-    "Prompt 1 for Group 3 - List 2",
-    "Prompt 2 for Group 3 - List 2",
-    // Add more prompts for List 2
-];
-
-let g3Prompts3 = [
-    "Prompt 1 for Group 3 - List 3",
-    "Prompt 2 for Group 3 - List 3",
-    // Add more prompts for List 3
-];
-
-//g4
-let g4Prompts1 = [
-    "Prompt 1 for Group 4 - List 1",
-    "Prompt 2 for Group 4 - List 1",
-    // Add more prompts for List 1
-];
-
-let g4Prompts2 = [
-    "Prompt 1 for Group 4 - List 2",
-    "Prompt 2 for Group 4 - List 2",
-    // Add more prompts for List 2
-];
-
-let g4Prompts3 = [
-    "Prompt 1 for Group 4 - List 3",
-    "Prompt 2 for Group 4 - List 3",
-    // Add more prompts for List 3
-];
-
-//g5
-let g5Prompts1 = [
-    "Prompt 1 for Group 5 - List 1",
-    "Prompt 2 for Group 5 - List 1",
-    // Add more prompts for List 1
-];
-
-let g5Prompts2 = [
-    "Prompt 1 for Group 5 - List 2",
-    "Prompt 2 for Group 5 - List 2",
-    // Add more prompts for List 2
-];
-
-let g5Prompts3 = [
-    "Prompt 1 for Group 5 - List 3",
-    "Prompt 2 for Group 5 - List 3",
-    // Add more prompts for List 3
-];
-
-// Event listeners for "Generate Prompt" buttons
-
-//g1
-document.getElementById("generateButton-g1-1").addEventListener("click", function () {
-    generateRandomPrompt(g1Prompts1);
-});
-
-document.getElementById("generateButton-g1-2").addEventListener("click", function () {
-    generateRandomPrompt(g1Prompts2);
-});
-
-document.getElementById("generateButton-g1-3").addEventListener("click", function () {
-    generateRandomPrompt(g1Prompts3);
-});
-
-//g2
-document.getElementById("generateButton-g2-1").addEventListener("click", function () {
-    generateRandomPrompt(g2Prompts1);
-});
-
-document.getElementById("generateButton-g2-2").addEventListener("click", function () {
-    generateRandomPrompt(g2Prompts2);
-});
-
-document.getElementById("generateButton-g2-3").addEventListener("click", function () {
-    generateRandomPrompt(g2Prompts3);
-});
-
-//g3
-document.getElementById("generateButton-g3-1").addEventListener("click", function () {
-    generateRandomPrompt(g3Prompts1);
-});
-
-document.getElementById("generateButton-g3-2").addEventListener("click", function () {
-    generateRandomPrompt(g3Prompts2);
-});
-
-document.getElementById("generateButton-g3-3").addEventListener("click", function () {
-    generateRandomPrompt(g3Prompts3);
-});
-
-//g4
-document.getElementById("generateButton-g4-1").addEventListener("click", function () {
-    generateRandomPrompt(g4Prompts1);
-});
-
-document.getElementById("generateButton-g4-2").addEventListener("click", function () {
-    generateRandomPrompt(g4Prompts2);
-});
-
-document.getElementById("generateButton-g4-3").addEventListener("click", function () {
-    generateRandomPrompt(g4Prompts3);
-});
-
-//g5
-document.getElementById("generateButton-g5-1").addEventListener("click", function () {
-    generateRandomPrompt(g5Prompts1);
-});
-
-document.getElementById("generateButton-g5-2").addEventListener("click", function () {
-    generateRandomPrompt(g5Prompts2);
-});
-
-document.getElementById("generateButton-g5-3").addEventListener("click", function () {
-    generateRandomPrompt(g5Prompts3);
-});
+const prompts = {
+    g1: {
+        list1: [
+            "Prompt 1 for Group 1 - List 1",
+            "Prompt 2 for Group 1 - List 1",
+            // Add more prompts for List 1
+        ],
+        list2: [
+            "Prompt 1 for Group 1 - List 2",
+            "Prompt 2 for Group 1 - List 2",
+            // Add more prompts for List 2
+        ],
+        list3: [
+            "Prompt 1 for Group 1 - List 3",
+            "Prompt 2 for Group 1 - List 3",
+            // Add more prompts for List 3
+        ],
+    },
+    g2: {
+        list1: [
+            "Prompt 1 for Group 2 - List 1",
+            "Prompt 2 for Group 2 - List 1",
+            // Add more prompts for List 1
+        ],
+        list2: [
+            "Prompt 1 for Group 2 - List 2",
+            "Prompt 2 for Group 2 - List 2",
+            // Add more prompts for List 2
+        ],
+        list3: [
+            "Prompt 1 for Group 2 - List 3",
+            "Prompt 2 for Group 2 - List 3",
+            // Add more prompts for List 3
+        ],
+    },
+    g3: {
+        list1: [
+            "Prompt 1 for Group 3 - List 1",
+            "Prompt 2 for Group 3- List 1",
+            // Add more prompts for List 1
+        ],
+        list2: [
+            "Prompt 1 for Group 3 - List 2",
+            "Prompt 2 for Group 3 - List 2",
+            // Add more prompts for List 2
+        ],
+        list3: [
+            "Prompt 1 for Group 3 - List 3",
+            "Prompt 2 for Group 3 - List 3",
+            // Add more prompts for List 3
+        ],
+    }, 
+    g4: {
+        list1: [
+            "Prompt 1 for Group 4 - List 1",
+            "Prompt 2 for Group 4- List 1",
+            // Add more prompts for List 1
+        ],
+        list2: [
+            "Prompt 1 for Group 4 - List 2",
+            "Prompt 2 for Group 4 - List 2",
+            // Add more prompts for List 2
+        ],
+        list3: [
+            "Prompt 1 for Group 4 - List 3",
+            "Prompt 2 for Group 4 - List 3",
+            // Add more prompts for List 3
+        ],
+    }, 
+    g5: {
+        list1: [
+            "Prompt 1 for Group 5 - List 1",
+            "Prompt 2 for Group 5- List 1",
+            // Add more prompts for List 1
+        ],
+        list2: [
+            "Prompt 1 for Group 5 - List 2",
+            "Prompt 2 for Group 5 - List 2",
+            // Add more prompts for List 2
+        ],
+        list3: [
+            "Prompt 1 for Group 5 - List 3",
+            "Prompt 2 for Group 5 - List 3",
+            // Add more prompts for List 3
+        ],
+    }, 
+    // Define prompts for other groups similarly
+};
 
 // Initialize the "Generate Prompt" button as disabled by default
 const generateButton = document.getElementById("generateButton");
@@ -170,100 +96,27 @@ generateButton.disabled = true; // Disable it by default
 const groupSelection = document.getElementById("groupSelection");
 groupSelection.addEventListener("change", function () {
     const selectedGroup = groupSelection.value;
-    // Hide all groups
-    const allGroups = document.querySelectorAll(".group-container");
-    allGroups.forEach(group => group.style.display = "none");
-    // Show the selected group
-    const selectedGroupDiv = document.getElementById(`group-${selectedGroup}`);
-    selectedGroupDiv.style.display = "block";
-    // Clear the click count and reset logged prompts
-    clickCount = 0;
-    loggedPrompts.length = 0;
-    clickCountDisplay.textContent = "Click count: 0";
-    promptDisplay.textContent = "";
-    exportMessage.textContent = "";
-    hearButton.disabled = true;
-    // Update the current prompts based on the selected group
-      switch (selectedGroup) {
-        case "g1":
-            switch (selectedList) {
-                case "list1":
-                    currentPrompts = g1Prompts1;
-                    break;
-                case "list2":
-                    currentPrompts = g1Prompts2;
-                    break;
-                case "list3":
-                    currentPrompts = g1Prompts3;
-                    break;
-            }
-            break;
-        case "g2":
-            switch (selectedList) {
-                case "list1":
-                    currentPrompts = g2Prompts1;
-                    break;
-                case "list2":
-                    currentPrompts = g2Prompts2;
-                    break;
-                case "list3":
-                    currentPrompts = g2Prompts3;
-                    break;
-            }
-            break;
-        case "g3":
-            switch (selectedList) {
-                case "list1":
-                    currentPrompts = g3Prompts1;
-                    break;
-                case "list2":
-                    currentPrompts = g3Prompts2;
-                    break;
-                case "list3":
-                    currentPrompts = g3Prompts3;
-                    break;
-            }
-            break;
-        case "g4":
-            switch (selectedList) {
-                case "list1":
-                    currentPrompts = g4Prompts1;
-                    break;
-                case "list2":
-                    currentPrompts = g4Prompts2;
-                    break;
-                case "list3":
-                    currentPrompts = g4Prompts3;
-                    break;
-            }
-            break;
-        case "g5":
-            switch (selectedList) {
-                case "list1":
-                    currentPrompts = g5Prompts1;
-                    break;
-                case "list2":
-                    currentPrompts = g5Prompts2;
-                    break;
-                case "list3":
-                    currentPrompts = g5Prompts3;
-                    break;
-            }
-            break;
-        default:
-            // Handle the case where none of the groups match (if needed)
-    }
+    const selectedList = "list1"; // You can change this to the desired list initially
+
+    // Update the current prompts based on the selected group and list
+    const currentPrompts = prompts[selectedGroup][selectedList];
 
     // Enable the "Generate Prompt" button when a group is selected
     generateButton.disabled = false;
 });
 
 let clickCount = 0;
-let loggedPrompts = [];
 const clickCountDisplay = document.getElementById("clickCountDisplay");
 const promptDisplay = document.getElementById("promptDisplay");
-const timestampList = document.getElementById("timestampList");
-const hearButton = document.getElementById("hearButton");
+const exportMessage = document.getElementById("exportMessage");
+
+generateButton.addEventListener("click", function () {
+    const selectedGroup = groupSelection.value;
+    const selectedList = "list1"; // You can change this to the desired list initially
+    const currentPrompts = prompts[selectedGroup][selectedList];
+
+    generateRandomPrompt(currentPrompts);
+});
 
 function generateRandomPrompt(prompts) {
     if (prompts.length === 0) {
@@ -272,81 +125,8 @@ function generateRandomPrompt(prompts) {
         clickCount++;
         const randomIndex = Math.floor(Math.random() * prompts.length);
         const randomPrompt = prompts[randomIndex];
-        const timestamp = new Date().toLocaleTimeString();
-
-        timestampList.innerHTML = "";
 
         promptDisplay.textContent = `Prompt #${clickCount}: ${randomPrompt}`;
-        const timestampItem = document.createElement("li");
-        timestampItem.textContent = `${clickCount}: ${randomPrompt} (Generated at ${timestamp})`;
-        timestampItem.style.fontFamily = "Roboto Mono, monospace";
-        timestampList.appendChild(timestampItem);
-
-        loggedPrompts.push({ prompt: randomPrompt, timestamp: timestamp });
-        prompts.splice(randomIndex, 1);
         clickCountDisplay.textContent = `Click count: ${clickCount}`;
-
-        // Enable the "Hear This Prompt" button after generating a prompt
-        hearButton.disabled = false;
     }
-}
-
-function saveLoggedData() {
-    if (loggedPrompts.length > 0) {
-        let csvData = "Prompt Name,Time Stamp\n";
-        for (const loggedPrompt of loggedPrompts) {
-            csvData += `"${loggedPrompt.prompt}","${loggedPrompt.timestamp}"\n`;
-        }
-        csvData += `Total Count,${clickCount}\n`;
-
-        exportData(csvData);
-    }
-}
-
-function exportData(data) {
-    const blob = new Blob([data], { type: 'text/csv' });
-    const url = window.URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = 'generated_data.csv';
-
-    document.body.appendChild(a);
-    a.click();
-
-    window.URL.revokeObjectURL(url);
-
-    const exportMessage = document.getElementById("exportMessage");
-    exportMessage.textContent = "Data exported successfully!";
-    exportMessage.style.color = "#556f7b";
-}
-
-function hearCurrentPrompt() {
-    const currentPromptText = promptDisplay.textContent.replace(/^Prompt \d+: /, ''); // Remove "Prompt #X: "
-    speakPrompt(currentPromptText);
-}
-
-const saveButton = document.getElementById("saveButton");
-const exportMessage = document.getElementById("exportMessage"); // Reference to the "Exported Data Message"
-
-saveButton.addEventListener("click", saveLoggedData);
-
-// Add a click event listener to the "Hear This Prompt" button
-hearButton.addEventListener("click", hearCurrentPrompt);
-
-// Disable the "Hear This Prompt" button initially
-hearButton.disabled = true;
-
-function isSpeechSynthesisSupported() {
-    return 'speechSynthesis' in window;
-}
-
-function speakPrompt(promptText) {
-    if (isSpeechSynthesisSupported()) {
-        const utterance = new SpeechSynthesisUtterance(promptText);
-        speechSynthesis.speak(utterance);
-    } else {
-        alert("Speech synthesis is not supported in this browser.");
-    }
-}
+});
