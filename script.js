0240060186ed5eb97914bac7819ff0e9f0d9d977
Muhammad Daniyal tariq <97436711+daniyalt0@users.noreply.github.com>
2023-09-14 05:58:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -201,4 +201,10 @@ function isSpeechSynthesisSupported() {
 }
 
 function speakPrompt(promptText) {
-    if
+    if (isSpeechSynthesisSupported()) {
+        const utterance = new SpeechSynthesisUtterance(promptText);
+        speechSynthesis.speak(utterance);
+    } else {
+        alert("Speech synthesis is not supported in this browser.");
+    }
+}
